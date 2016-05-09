8ed1e1ada527b6670a5116d575e80042cc3a8c1d
loonggg <loonggg@qq.com>
2016-05-09 16:00:48 +0800

    圆环进度显示
    圆环进度显示

---
+++ CircleProgressBar/app/src/main/java/com/loonggg/godcoder/MainActivity.java
@@ -0,0 +1,19 @@
+package com.loonggg.godcoder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.loonggg.godcoder.view.CircleProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    private CircleProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = (CircleProgressBar) findViewById(R.id.pb);
+        progressBar.setProgress(1f,5000);
+
+    }
+}
